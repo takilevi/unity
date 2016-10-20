cd5524105c79f32a674e2b237d9f97e43a404f0c
takilevi <levi.takacs@gmail.com>
2016-10-20 20:02:25 +0200

    zombie_cica
    a néni még nem tud mozogni

---
+++ ZombieConga/Assets/Scripts/ZombieController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float turnSpeed;
+
+	private Vector3 moveDirection;
+
+
+	// Use this for initialization
+	void Start () {
+		moveDirection = Vector3.right;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// 1
+		Vector3 currentPosition = transform.position;
+		// 2
+		if( Input.GetButton("Fire1") ) {
+			// 3
+			Vector3 moveToward = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+			// 4
+			moveDirection = moveToward - currentPosition;
+			moveDirection.z = 0; 
+			moveDirection.Normalize();
+		}
+
+		Vector3 target = moveDirection * moveSpeed + currentPosition;
+		transform.position = Vector3.Lerp( currentPosition, target, Time.deltaTime );
+
+		float targetAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+		transform.rotation = 
+			Quaternion.Slerp( transform.rotation, 
+			                 Quaternion.Euler( 0, 0, targetAngle ), 
+			                 turnSpeed * Time.deltaTime );
+	}
+}
