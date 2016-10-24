6c0d2cd9ab01ba3d4fef8e7c02b436ee0205df4f
takilevi <levi.takacs@gmail.com>
2016-10-24 19:18:55 +0200

    elso konzi elotti anyag

---
+++ ZombieConga/Assets/Scripts/KittyCreator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class KittyCreator : MonoBehaviour
+{
+    //1
+    public float minSpawnTime = 0.75f;
+    public float maxSpawnTime = 2f;
+    public GameObject catPrefab;
+    //2    
+    void Start()
+    {
+        Invoke("SpawnCat", minSpawnTime);
+    }
+
+    //3
+    void SpawnCat()
+    {
+        Debug.Log("TODO: Birth a cat at " + Time.timeSinceLevelLoad);
+        Invoke("SpawnCat", Random.Range(minSpawnTime, maxSpawnTime));
+    }
+}
--- ZombieConga/Assets/Scripts/ZombieController.cs
+++ ZombieConga/Assets/Scripts/ZombieController.cs
@@ -37,9 +37,42 @@ public class ZombieController : MonoBehaviour {
 			Quaternion.Slerp( transform.rotation, 
 			                 Quaternion.Euler( 0, 0, targetAngle ), 
 			                 turnSpeed * Time.deltaTime );
-	}
+        EnforceBounds();
+
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Hit " + other.gameObject);
+    }
+    private void EnforceBounds()
+    {
+        // 1
+        Vector3 newPosition = transform.position;
+        Camera mainCamera = Camera.main;
+        Vector3 cameraPosition = mainCamera.transform.position;
+
+        // 2
+        float xDist = mainCamera.aspect * mainCamera.orthographicSize;
+        
+        float xMax = cameraPosition.x + xDist;
+        float xMin = cameraPosition.x - xDist;
+        
+
+        // 3
+        if (newPosition.x < xMin || newPosition.x > xMax)
+        {
+            newPosition.x = Mathf.Clamp(newPosition.x, xMin, xMax);
+            moveDirection.x = -moveDirection.x;
+        }
+        // TODO vertical bounds
+        float yMax = mainCamera.orthographicSize;
+
+        if (newPosition.y < -yMax || newPosition.y > yMax)
+        {
+            newPosition.y = Mathf.Clamp(newPosition.y, -yMax, yMax);
+            moveDirection.y = -moveDirection.y;
+        }
+        // 4
+        transform.position = newPosition;
     }
 }
