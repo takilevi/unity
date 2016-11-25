fb463d673a375ba56f52c366a742a6bba3e59aab
takilevi <levi.takacs@gmail.com>
2016-11-25 14:52:46 +0100

    three enemies spawn randomly

--- second_try_game/Assets/Scripts/EnemyCreator.cs
+++ second_try_game/Assets/Scripts/EnemyCreator.cs
@@ -6,10 +6,10 @@ public class EnemyCreator : MonoBehaviour {
 	//1
 	public float minSpawnTime = 0.3f; 
 	public float maxSpawnTime = 2f; 
-	public GameObject enemyPrefab;
-	//public GameObject enemyPrefab1;
-	//public GameObject enemyPrefab2;
-	//public GameObject enemyPrefab3;
+	//private GameObject enemyPrefab;
+	public GameObject enemyPrefab1;
+	public GameObject enemyPrefab2;
+	public GameObject enemyPrefab3;
 	//2    
 	void Start () {
 		Invoke("SpawnEnemy",minSpawnTime);
@@ -26,17 +26,38 @@ public class EnemyCreator : MonoBehaviour {
 		float yMax = camera.orthographicSize - 0.2f;
 		
 		//Random rnd = new Random();
-		int enemy = Random.Range(1, 3);
+		int enemy = Random.Range(1, 4);
 		Debug.Log (enemy);
+		if (enemy == 1) {
 
-		Vector3 enemyPos = 
+			Vector3 enemyPos = 
 			new Vector3(cameraPos.x + Random.Range(xMax - xRange, xMax),
 			            Random.Range(-yMax, yMax),
-			            enemyPrefab.transform.position.z);
+			            enemyPrefab1.transform.position.z);
 		
-		// 3
-		Instantiate(enemyPrefab, enemyPos, Quaternion.identity);
+			Instantiate(enemyPrefab1, enemyPos, Quaternion.identity);
 
-		Invoke("SpawnEnemy", Random.Range(minSpawnTime, maxSpawnTime));
+			Invoke("SpawnEnemy", Random.Range(minSpawnTime, maxSpawnTime));
+		}
+		if (enemy == 2) {
+			Vector3 enemyPos = 
+				new Vector3(cameraPos.x + Random.Range(xMax - xRange, xMax),
+				            Random.Range(-yMax, yMax),
+				            enemyPrefab2.transform.position.z);
+			
+			Instantiate(enemyPrefab2, enemyPos, Quaternion.identity);
+			
+			Invoke("SpawnEnemy", Random.Range(minSpawnTime, maxSpawnTime));
+		}
+		if (enemy == 3) {
+			Vector3 enemyPos = 
+				new Vector3(cameraPos.x + Random.Range(xMax - xRange, xMax),
+				            Random.Range(-yMax, yMax),
+				            enemyPrefab3.transform.position.z);
+			
+			Instantiate(enemyPrefab3, enemyPos, Quaternion.identity);
+			
+			Invoke("SpawnEnemy", Random.Range(minSpawnTime, maxSpawnTime));
+		}
 	}
 }
